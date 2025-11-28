5d13d1f862801d8a2aff8400d1841d32e01d0bb7
Ayush Bamrara <ayushbamrara10@gmail.com>
2025-11-28 22:44:59 +0530

    LocalHost fix

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -12,7 +12,7 @@ function Login () {
     const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "http://localhost:3000/dashboard"
+      redirectTo: `${NEXT_PUBLIC_BASE_URL}/dashboard`
     }
   });
     
